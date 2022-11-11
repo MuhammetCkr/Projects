e67864bafc9d20d013b9530827e6a3f8982b3195
MuhammetCkr <muhammetckrr@gmail.com>
2022-11-11 13:47:55 +0300

    MiniShop Eklendi

---
+++ MiniShop/MiniShop.Web/Models/PasswordModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniShop.Web.Models
+{
+    public class PasswordModel
+    {
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunlu.")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunlu.")]
+        //[DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunlu.")]
+        //[DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor!")]
+        public string RePassword { get; set; }
+    }
+}
