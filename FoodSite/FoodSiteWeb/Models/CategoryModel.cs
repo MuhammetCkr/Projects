fc4f3a195b35df2c6b23ad3d76c3ee9d4045b45b
MuhammetCkr <muhammetckrr@gmail.com>
2022-11-23 16:53:04 +0300

    23.11.2022

--- FoodSite/FoodSiteWeb/Models/CategoryModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using FoodSite.Entity;
-
-namespace FoodSiteWeb.Models
-{
-    public class CategoryModel : BaseModel
-    {
-        public List<Product> Products { get; set; }
-    }
-}
--- FoodSite/FoodSiteWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FoodSiteWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
